5c61ed40f0dd4055367f16f27f66d135d672a406
anushasonte <anusha.sonte@gmail.com>
2023-12-29 10:16:11 -0600

    time complexity of string reverse

--- Recursion/PrintStringInReverse.java
+++ Recursion/PrintStringInReverse.java
@@ -1,4 +1,19 @@
 package Recursion;
+/*
+ Time complexity: 
+ O(n) -> number of insertions in stack = length of input string 
+
+Stack:
+input: anusha
+
+reverseString("anusha",0) : prints "a" : return
+reverseString("anusha",1) : prints "n" : reverseString("anusha",0) : return 
+reverseString("anusha",2) : prints "u" : reverseString("anusha",1) : return
+reverseString("anusha",3) : prints "s" : reverseString("anusha",2) : return
+reverseString("anusha",4) : prints "h" : reverseString("anusha",3) : return
+reverseString("anusha",5) : prints "a" : reverseString("anusha",4) : return
+main(): reverseString("anusha",5) : returns ahsuna
+*/
 import java.util.*;
 
 public class PrintStringInReverse {
