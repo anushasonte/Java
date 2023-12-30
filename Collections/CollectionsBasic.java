eb73f492b9b942846fffac9c744ebf45a3321ace
anushasonte <anusha.sonte@gmail.com>
2023-12-30 13:01:05 -0600

    collections framework basic

---
+++ Collections/CollectionsBasic.java
@@ -0,0 +1,54 @@
+package Collections;
+
+/*
+Different types of data needs to be stored in different ways to optimize the storage and retrieval.
+Collections framework has inbuilt datastructures and we can use them. 
+It is a collection of classes and interfaces.
+
+Inferfaces: 
+Iterable - main
+Collection class implements Iterable interface. 
+Collection has three main interfaces:
+1. List
+2. Queue
+3. Set
+The above three interfaces/datastructures can be implemented by classes as they cannot be used directly.
+
+Methods on interfaces:
+1. add() : add an obj 
+2. size() : gets size of that collection
+3. remove() : removes an obj
+4. iterate() : to iterate over the collection
+5. addAll() : adds multiple objects at once.
+6. removeAll(): removes multiple objects at once.
+7. clear() : clears the collection
+
+
+List interface:
+3 classes:
+1. ArrayList : just like an array but of variable size.
+2. Linkedlist : chaining of multiple elements
+3. Vector class : similar to linkedlist, but is thread-safe. extended by Stack (LIFO) - class
+
+Queue interface: (FIFO)
+3 classes:
+1. Priority queue 
+2. Linkedlist
+3. Dequeue Interface -> double ended queue! ArrayDequeue class
+
+Set interface: Collection of unique objects!
+1. Hashset class
+2. LinkedHashset class
+3. SortedSet interface -> Treeset class -> for sorted arrangement
+
+Map interface: Stored in the form of key-value pairs
+1. Hashmap clas
+2. LinkedHashMap class
+3. SortedMap interface -> Treemap class - sorted as per key
+4. HashTable
+
+*/
+
+public class CollectionsBasic {
+    
+}
