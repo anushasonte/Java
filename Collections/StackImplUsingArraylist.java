6b509824fc7ae105f364e1ec5d4a0978478a134f
anushasonte <anusha.sonte@gmail.com>
2024-01-03 18:10:29 -0600

    Stack implementation using linkedlist

---
+++ Collections/StackImplUsingArraylist.java
@@ -0,0 +1,64 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class StackImplUsingArraylist {
+
+   static class Stack {
+        static ArrayList <Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty(){
+            if(list.size() == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public static void push(int data){
+            list.add(data);
+        }
+
+        public static int pop(){
+            int data = -1;
+            if(!isEmpty()){
+                data = list.get(list.size()-1);
+                list.remove(list.size()-1); //removing last element from list
+            }
+            return data;
+        }
+
+        public static int peep(){
+            int data = -1;
+            if(!isEmpty()){
+                data = list.get(list.size()-1);
+            }
+            return data;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Pushing data into Stack..");
+        Stack.push(1);
+        Stack.push(2);
+        Stack.push(3);
+        Stack.push(4);
+        Stack.push(5);
+
+        System.out.println("Retrieving top most element..");
+        System.out.println(Stack.peep());
+
+        System.out.println("Popping elements from Stack..");
+        while(!Stack.isEmpty()){
+            System.out.println(Stack.pop());
+        }
+
+        System.out.println("Retrieving top most element..");
+        System.out.println(Stack.peep());
+        
+    }
+    
+}
---
+++ Collections/StackImplUsingLinkedlist.java
@@ -0,0 +1,95 @@
+package Collections;
+
+//STACK - LIFO data structure!
+//PUSH, POP, PEEK OPERATIONS
+
+//Push - adding element into Stack : O(1)
+//Pop - removing top most element from Stack : O(1)
+//peek - retrieve top element from Stack : O(1)
+
+//stackfull() - stackoverflow and stackEmpty() functions before pushing and popping elements from stack! 
+
+//Implementation:
+//Using Array! : fixed size!
+//Using Linkedlist - variable size ; new element added will be head. 
+//Using Arraylist - variable size
+
+public class StackImplUsingLinkedlist {
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            next = null;
+
+        }
+    }
+
+    static class Stack {
+        public static Node head;
+
+        public static boolean isStackEmpty(){
+            if(head == null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public static void push(int data) {
+            Node newNode = new Node(data);
+            if(isStackEmpty()){
+                head = newNode;
+            }
+            else{
+                newNode.next = head;
+                head = newNode;
+            }
+
+        }
+
+        public static int pop(){
+            int top = -1;
+            if(!isStackEmpty()){
+                top = head.data;
+                head = head.next;
+            }
+            return top;
+        }
+
+        public static int peep(){
+            int top = -1;
+            if(!isStackEmpty()){
+                top = head.data;
+            }
+            return top;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Pushing data into Stack..");
+        Stack.push(1);
+        Stack.push(2);
+        Stack.push(3);
+        Stack.push(4);
+        Stack.push(5);
+
+        System.out.println("Retrieving top most element..");
+        System.out.println(Stack.peep());
+
+        System.out.println("Popping elements from Stack..");
+        while(!Stack.isStackEmpty()){
+            System.out.println(Stack.pop());
+        }
+
+        System.out.println("Retrieving top most element..");
+        System.out.println(Stack.peep());
+
+    }
+
+}
