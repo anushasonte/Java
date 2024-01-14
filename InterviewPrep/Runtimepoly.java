9f18e237995af106a6d082ea619a93dc6fffd071
anushasonte <anusha.sonte@gmail.com>
2024-01-14 10:14:13 -0600

    Create Runtimepoly.java

---
+++ InterviewPrep/Runtimepoly.java
@@ -0,0 +1,35 @@
+package InterviewPrep;
+
+class Shape {
+    void draw(){
+        System.out.println("Drawing a shape!");
+    }
+}
+
+class Circle extends Shape { //Object to Object inheritance
+    void draw(){
+        System.out.println("Drawing a Circle!");
+    }
+}
+
+class Rectangle extends Shape {
+    void draw(){
+        System.out.println("Drawing a Rectangle!");
+    }
+}
+
+public class Runtimepoly {
+
+    public static void main(String[] args) {
+
+        //Compile time polymorphism
+        Circle c = new Circle();
+        c.draw(); //call is resolved at compile time.
+
+        //Run time polymorphism: Ref variable of parent class is used to create child class
+        Shape s = new Circle();
+        s.draw();  //Determination of the method call is based on the obj being referenced. Call is resolved at runtime.
+        
+    }
+    
+}
